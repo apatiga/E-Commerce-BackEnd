ab5c75bf325c4be91ba30e21a487e606b0a79862
apatiga <128877342+apatiga@users.noreply.github.com>
2023-07-06 20:08:49 -0700

    got TA help and changed hot from local host to 127 which ficed issue when runnning npm run seed

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+console.log("test", process.env.DB_NAME,process.env.DB_USER)
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, null, {
+      host: '127.0.0.1',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -16,21 +16,59 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with this id!" });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json({ message: "Tag not found!" });
+  }
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json({ message: "Tag creation failed" });
+  }
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updated = await Tag.update(req.body, {
+      where: { id: req.params.id },
+    });
+    !updated[0]
+      ? res.status(404).json({ message: "No tag found with this id!" })
+      : res.status(200).json(updated);
+  } catch (err) {
+    res.status(500).json({ message: "Tag update failed" });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleted = await Tag.destroy({ where: { id: req.params.id } });
+    !deleted
+      ? res.status(404).json({ message: "No tag found with this id!" })
+      : res.status(200).json(deleted);
+  } catch (err) {
+    res.status(500).json({ message: "Tag delete failed" });
+  }
 });
 
 module.exports = router;
